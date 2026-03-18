c605887bdee34c59a4861f82427f2d7ded32bfaa
dimasyoga42 <dimasyoga9090@gmail.com>
2026-03-18 22:38:13 +0700

    refector command

--- plugins/fun/loli.js
+++ plugins/fun/loli.js
@@ -1,1 +1,20 @@
-const handler = (m, { conn }) => {};
+import { config } from "../../config.js";
+import { sendFancyTextModif, sendImage } from "../../src/config/message.js";
+
+const handler = (m, { conn }) => {
+  try {
+    sendImage(
+      conn,
+      m.chat,
+      `https://api.deline.web.id/random/loli`,
+      "ini loli anda",
+      m,
+    );
+  } catch (err) {}
+};
+
+handler.command = "loli";
+handler.alias = ["pdf"];
+handler.category = "Menu Fun";
+handler.submenu = "Fun";
+export default handler;
