1c5d404dccf6da9f760c0b71ae38f3343018e546
dimasyoga42 <dimasyoga9090@gmail.com>
2026-03-18 09:05:39 +0700

    feat: penambahan mq

--- config.js
+++ config.js
@@ -110,244 +110,243 @@ export const config = {
   303-305 *Red Ash Dragon Rudis (Ultimate)* -> Espuma Dome: Entrance
   `,
   upbag: `
-  *50 => 51*
-  - 1x Kulit Colon (Colon - Tanah Pembangunan)
+*50 => 51*
+- 1x Kulit Colon (Colon - Tanah Pembangunan)
 
-  *51 => 52*
-  - 1x Kulit Berkualitas (Lavarca - Dataran Rakau)
+*51 => 52*
+- 1x Kulit Berkualitas (Lavarca - Dataran Rakau)
 
-  *52 => 53*
-  Spina 1.000
+*52 => 53*
+- Spina 1.000
 
-  *53 => 54*
-  - 1x Kulit Minotaur (Minotaur - Kuil Runtuh:Area Terlarang)
-  - 1x Pecahan Kristal Jingga (Cobre - Danau Icule)
+*53 => 54*
+- 1x Kulit Minotaur (Minotaur - Kuil Runtuh:Area Terlarang)
+- 1x Pecahan Kristal Jingga (Cobre - Danau Icule)
 
-  *54 => 55*
-  - 1x Kulit Anjing Hutan (Anjing Hutan - Hutan Marbaro:Bagian Dalam)
-  - 1x Lencana Goblin (Goblin Bos - Gua Ribisco:Bagian Dalam)
+*54 => 55*
+- 1x Kulit Anjing Hutan (Anjing Hutan - Hutan Marbaro:Bagian Dalam)
+- 1x Lencana Goblin (Goblin Bos - Gua Ribisco:Bagian Dalam)
 
-  *55 => 56*
-  Spina 2.000
+*55 => 56*
+- Spina 2.000
 
-  *56 => 57*
-  - 1x Bulu Mochelo (Mochelo - Lereng Merapi: A3)
-  - 10x Kain Linen (Crow Killer - Dusun Douce)
+*56 => 57*
+- 1x Bulu Mochelo (Mochelo - Lereng Merapi: A3)
+- 10x Kain Linen (Crow Killer - Dusun Douce)
 
-  *57 => 58*
-  - 1X Bulu Naga Giok (Forestia - Tanah Kaos)
-  - 10x Tanduk Berkualitas (Bandot - Tanah Tinggi Yorl)
+*57 => 58*
+- 1X Bulu Naga Giok (Forestia - Tanah Kaos)
+- 10x Tanduk Berkualitas (Bandot - Tanah Tinggi Yorl)
 
-  *58 => 59*
-  - 1x Sabuk Boss Roga (BosS Roga - Kawah Saham:Bagian Dalam)
-  - 10x Kain Beludu (Orc - Kawah Saham)
+*58 => 59*
+- 1x Sabuk Boss Roga (BosS Roga - Kawah Saham:Bagian Dalam)
+- 10x Kain Beludu (Orc - Kawah Saham)
 
-  *59 => 60*
-  Spina 4.000
+*59 => 60*
+- Spina 4.000
 
-  *60 => 61*
-  - 2x Cakar Beruang (Violaccon - Padang Darkanon)
-  - 20x Sheeting Fabric (Cassy - Makam Ratu Kuno)
+*60 => 61*
+- 2x Cakar Beruang (Violaccon - Padang Darkanon)
+- 20x Sheeting Fabric (Cassy - Makam Ratu Kuno)
 
-  *61 => 62*
-  - 2x Rantai Kukuh (Pendekar Bertopeng - Tanah Pertanian:Tanah Tinggi)
-  - 20x Kain polister (Boneka Pengembara - Kota Hilang)
+*61 => 62*
+- 2x Rantai Kukuh (Pendekar Bertopeng - Tanah Pertanian:Tanah Tinggi)
+- 20x Kain polister (Boneka Pengembara - Kota Hilang)
 
-  *62 => 63*
-  - 2x Sisik Naga Sabana (Naga Sabana Yelb - Desa Albatif)
-  - 20x Kulit Serigala Alien (Serigala Luar - Gerbang Dunia Lain)
+*62 => 63*
+- 2x Sisik Naga Sabana (Naga Sabana Yelb - Desa Albatif)
+- 20x Kulit Serigala Alien (Serigala Luar - Gerbang Dunia Lain)
 
-  *63 => 64*
-  Spina 8.000
+*63 => 64*
+- Spina 8.000
 
-  *64 => 65*
-  - 2x Jubah Sobek (Goovua - Gurun Akaku:Bukit)
-  - 20x Kulit Tupai (Rodentail - Maia Deila)
+*64 => 65*
+- 2x Jubah Sobek (Goovua - Gurun Akaku:Bukit)
+- 20x Kulit Tupai (Rodentail - Maia Deila)
 
-  *65 => 66*
-  - 2x Tanduk Elang Zamrud (Elang Zamrud - Teras kerikil)
-  - 20x Bulu Kambing (Koza - Jurang Dungkel)
+*65 => 66*
+- 2x Tanduk Elang Zamrud (Elang Zamrud - Teras kerikil)
+- 20x Bulu Kambing (Koza - Jurang Dungkel)
 
-  *66 => 67*
-  - 2x Sayap Naga Senja (Naga Senja - Benteng Solfini:Atap)
-  - 20x Bulu Halus (Rabby/Little Snowbear - Lembah Es Polde)
+*66 => 67*
+- 2x Sayap Naga Senja (Naga Senja - Benteng Solfini:Atap)
+- 20x Bulu Halus (Rabby/Little Snowbear - Lembah Es Polde)
 
-  *67 => 68*
-  Spina 16.000
+*67 => 68*
+- Spina 16.000
 
-  *68 => 69*
-  - 2X Rantai Penyucian (Cerberus - Mata Air Kelahiaran:Puncak)
-  - 20x Kain Goyah (Jewel Eye - Mata Air Kelahiaran)
+*68 => 69*
+- 2X Rantai Penyucian (Cerberus - Mata Air Kelahiaran:Puncak)
+- 20x Kain Goyah (Jewel Eye - Mata Air Kelahiaran)
 
-  *69 => 70*
-  - 2x Benang Aranea (Aranea - Taman Sublimasi:Pusat)
-  - 20x Benang Laba-Laba kecil (Aramia - Taman Sublimasi)
+*69 => 70*
+- 2x Benang Aranea (Aranea - Taman Sublimasi:Pusat)
+- 20x Benang Laba-Laba kecil (Aramia - Taman Sublimasi)
 
-  *70 => 71*
-  - 3x kain Dewi Tiruan (Imitacia - Istana Gelap:Aula Besar)
-  - 10X Kain Apung (Flying Executioner - Buian Prajurit)
-  - 20x Tapak Lembut (Bunny Summoner - Sungai Kegelapan)
+*70 => 71*
+- 3x kain Dewi Tiruan (Imitacia - Istana Gelap:Aula Besar)
+- 10X Kain Apung (Flying Executioner - Buian Prajurit)
+- 20x Tapak Lembut (Bunny Summoner - Sungai Kegelapan)
 
-  *71 => 72*
-  - 3x Surai Hewan Iblis (Memecoleous - Istana Gelap:A2)
-  - 10x Bantalan Tapak keras (Manticore - Istana Gelap)
-  - 20x Bulu Bayangan Hitam (Shadow Fly - Istana Gelap)
+*71 => 72*
+- 3x Surai Hewan Iblis (Memecoleous - Istana Gelap:A2)
+- 10x Bantalan Tapak keras (Manticore - Istana Gelap)
+- 20x Bulu Bayangan Hitam (Shadow Fly - Istana Gelap)
 
-  *72 => 73*
-  Spina 32.000
+*72 => 73*
+- Spina 32.000
 
-  *73 => 74*
-  - 3x Bulu Tapir (Tapir - Graben Membara:Permukan)
-  - 10x Bulu Kaku (Graben Membara:Permukaan)
-  - 20x Minyak Anti-Karat (Ornis Demi Machina - Garis Pertahanan Artileri Otomatis)
+*73 => 74*
+- 3x Bulu Tapir (Tapir - Graben Membara:Permukan)
+- 10x Bulu Kaku (Graben Membara:Permukaan)
+- 20x Minyak Anti-Karat (Ornis Demi Machina - Garis Pertahanan Artileri Otomatis)
 
-  *74 => 75*
-  - 3x Kain Kuno (Proto Leon - Reruntuhan Singolare:L3)
-  - 10x Kulit Pohon Lunak (Floral Bee - Situs Simcracker)
-  - 20x Rambut Potum Kotor (Slum Potum - Klapse Kumuh)
+*74 => 75*
+- 3x Kain Kuno (Proto Leon - Reruntuhan Singolare:L3)
+- 10x Kulit Pohon Lunak (Floral Bee - Situs Simcracker)
+- 20x Rambut Potum Kotor (Slum Potum - Klapse Kumuh)
 
-  *75 => 76*
-  - 3x Tulang Raksasa Merah (Dusk Machina - Pabrik Demi Machina Kecil:A2)
-  - 10x Mantel Hitam Sobek (Rugos Demi Machina - Pabrik Demi Machina Kecil)
-  - 20x Rantai Putus (Machina Penyiksa - Pabrik Demi Machina Kecil)
+*75 => 76*
+- 3x Tulang Raksasa Merah (Dusk Machina - Pabrik Demi Machina Kecil:A2)
+- 10x Mantel Hitam Sobek (Rugos Demi Machina - Pabrik Demi Machina Kecil)
+- 20x Rantai Putus (Machina Penyiksa - Pabrik Demi Machina Kecil)
 
-  *76 => 77*
-  - 3X Sisik Chimera (Mozto Machina - Pabrik Demi machina Besar:Bagian Terdalam)
-  - 10X Benda Pendar Aneh (Horn Machina - Pabrik Demi Machina Besar)
-  - 20x Tentakel Tangguh (Ledon Machina - Pabrik Demi Machina Besar)
+*76 => 77*
+- 3X Sisik Chimera (Mozto Machina - Pabrik Demi machina Besar:Bagian Terdalam)
+- 10X Benda Pendar Aneh (Horn Machina - Pabrik Demi Machina Besar)
+- 20x Tentakel Tangguh (Ledon Machina - Pabrik Demi Machina Besar)
 
-  *77 => 78*
-  Spina 64.000
+*77 => 78*
+- Spina 64.000
 
-  *78 => 79*
-  - 3x Jubah Roh Hutan (Lalvada - Hutan Monster:Bagian Dalam)
-  - 10x Taring Tanaman (Nepenthe - Hutan Monster)
-  - 20x Kain Felt (Naga Boneka - Mansion Lufenas)
+*78 => 79*
+- 3x Jubah Roh Hutan (Lalvada - Hutan Monster:Bagian Dalam)
+- 10x Taring Tanaman (Nepenthe - Hutan Monster)
+- 20x Kain Felt (Naga Boneka - Mansion Lufenas)
 
-  *79 => 80*
-  - 3x Aloi Lyark (Gwaimol - Penjara Cuervo:Atap)
-  - 10x Baju Penjaga Robek (Sipir Lyark - Penjara Cuervo)
-  - 20x Kain Lembu (Lyark Spesialis - Laboratorium Brahe)
+*79 => 80*
+- 3x Aloi Lyark (Gwaimol - Penjara Cuervo:Atap)
+- 10x Baju Penjaga Robek (Sipir Lyark - Penjara Cuervo)
+- 20x Kain Lembu (Lyark Spesialis - Laboratorium Brahe)
 
-  *80 => 81*
-  - 4x Kain Bercahaya (Seraph Machina - Menara Penembus Bumi:Sisi Dalam)
-  - 20x Kulit Sintetis Rusak (Lyark Brawler - Sekitar Alun-alun Droma)
-  - 20x Cawat Pengeksekusi (Volo - Sekitar Alun-alun Droma)
+*80 => 81*
+- 4x Kain Bercahaya (Seraph Machina - Menara Penembus Bumi:Sisi Dalam)
+- 20x Kulit Sintetis Rusak (Lyark Brawler - Sekitar Alun-alun Droma)
+- 20x Cawat Pengeksekusi (Volo - Sekitar Alun-alun Droma)
 
-  *81 => 82*
-  - 4x Potongan Baju K. Kecil (Venena Coenubia - Istana Ultimea Takhta)
-  - 20x Pecahan Zirah Keras (High Tigris - Istana Ultimea)
-  - 20x Kulit Ular (Ular Kolam - Reservoir Copia)
+*81 => 82*
+- 4x Potongan Baju K. Kecil (Venena Coenubia - Istana Ultimea Takhta)
+- 20x Pecahan Zirah Keras (High Tigris - Istana Ultimea)
+- 20x Kulit Ular (Ular Kolam - Reservoir Copia)
 
-  *82 => 83*
-  Spina 100.000
+*82 => 83*
+- Spina 100.000
 
-  *83 => 84*
-  - 4X Kulit Mama Fluck (Mama Fluck - Gua Pelupa)
-  - 20x Daun Besar Colon (Leedle Colon - Dataran Rokoko)
-  - 20x Bulu Garis Vertikal (Rakun Tumbuhan - Hutan Curone)
+*83 => 84*
+- 4X Kulit Mama Fluck (Mama Fluck - Gua Pelupa)
+- 20x Daun Besar Colon (Leedle Colon - Dataran Rokoko)
+- 20x Bulu Garis Vertikal (Rakun Tumbuhan - Hutan Curone)
 
-  *84 => 85*
-  - 4x Kain Rohani Mardula (Mardula - Serambi Dewa Berkah)
-  - 20x Kain Berkilau Misterius (Malaikat Gelembung - Kuil Para Dewa)
-  - 20x Bulu Kelabu (Haliabubo - Reruntuhan G. Mithurna:Koridor Dasar)
+*84 => 85*
+- 4x Kain Rohani Mardula (Mardula - Serambi Dewa Berkah)
+- 20x Kain Berkilau Misterius (Malaikat Gelembung - Kuil Para Dewa)
+- 20x Bulu Kelabu (Haliabubo - Reruntuhan G. Mithurna:Koridor Dasar)
 
-  *85 => 86*
-  - 4x Mantel Carbuncle (Carbuncle - Serambi Dewa Pembangunan)
-  - 20x Kain Rajut (Bubble Angle - Koridor haresi)
-  - 20x Ekor Beruang Berkantong (Oddy - Kuil Para Dewa:A4)
+*85 => 86*
+- 4x Mantel Carbuncle (Carbuncle - Serambi Dewa Pembangunan)
+- 20x Kain Rajut (Bubble Angle - Koridor haresi)
+- 20x Ekor Beruang Berkantong (Oddy - Kuil Para Dewa:A4)
 
-  *86 => 87*
-  - 4x Bulu Raja Piton (Raja Piton - Pegunungan Elf:Kuil)
-  - 20x Bulu Putih lebat (Bandot - Taman Es & Salju)
-  - 20x Bulu Abu Kaku (Silveria - Pegunungan Elf)
+*86 => 87*
+- 4x Bulu Raja Piton (Raja Piton - Pegunungan Elf:Kuil)
+- 20x Bulu Putih lebat (Bandot - Taman Es & Salju)
+- 20x Bulu Abu Kaku (Silveria - Pegunungan Elf)
 
-  *87 => 88*
-  - 4x Ingot Kuno (Golem Preman - Kuil Naga kegelapan:Tengah)
-  - 20X Taring Serigala Es (Serigala Tanduk - Pegunungan Elf)
-  - 20x Kain Gelap (Soul Reaper - Kuil Naga Kegelapan)
+*87 => 88*
+- 4x Ingot Kuno (Golem Preman - Kuil Naga kegelapan:Tengah)
+- 20X Taring Serigala Es (Serigala Tanduk - Pegunungan Elf)
+- 20x Kain Gelap (Soul Reaper - Kuil Naga Kegelapan)
 
-  *88 => 89*
-  Spina 200.000
+*88 => 89*
+- Spina 200.000
 
-  *89 => 90*
-  - 4x Taring Tuscog (Tuscog - Jalan Eryldan:Sekitan Hutan Ein)
-  - 20x Larva Silk (Tikus Lumut - Hutan Ein)
-  - 20x Taring Manusia Serigala (Wolfie - Jalan Eryldan)
+*89 => 90*
+- 4x Taring Tuscog (Tuscog - Jalan Eryldan:Sekitan Hutan Ein)
+- 20x Larva Silk (Tikus Lumut - Hutan Ein)
+- 20x Taring Manusia Serigala (Wolfie - Jalan Eryldan)
 
-  *90 => 91*
-  - 5x Serpihan Kayu Kuzto (Kuzto - Distrik Labilans: Alun-alun)
-  - 20x Bulu Cerpelai (Sentreast - Distrik Fabriska)
-  - 30x Sabuk Pinggang Misterius (Moculus - Distrik Fractum)
+*90 => 91*
+- 5x Serpihan Kayu Kuzto (Kuzto - Distrik Labilans: Alun-alun)
+- 20x Bulu Cerpelai (Sentreast - Distrik Fabriska)
+- 30x Sabuk Pinggang Misterius (Moculus - Distrik Fractum)
 
-  *91 => 92*
-  - 5x Kantong kristal (Nemopinaria - Distrik Recetacula:A1)
-  - 20x Ekor Lembu (Alpoca - Distrik Labilans)
-  - 30x Papula Kuat (Toksinaria - Distrik Recetacula)
+*91 => 92*
+- 5x Kantong kristal (Nemopinaria - Distrik Recetacula:A1)
+- 20x Ekor Lembu (Alpoca - Distrik Labilans)
+- 30x Papula Kuat (Toksinaria - Distrik Recetacula)
 
-  *92 => 93*
-  - 5x Sayap Repthon (Repthon - Zona Riset Deltzon:Area terdalam)
-  - 20 Kancing Polong (Colon Marquis - Reruntuhan Mansion Lufenas Tua)
-  - 30x Kain Perca Jas Panjang (Gulingkar - Zona Riset Deltzon)
+*92 => 93*
+- 5x Sayap Repthon (Repthon - Zona Riset Deltzon:Area terdalam)
+- 20 Kancing Polong (Colon Marquis - Reruntuhan Mansion Lufenas Tua)
+- 30x Kain Perca Jas Panjang (Gulingkar - Zona Riset Deltzon)
 
-  *93 => 94*
-  - 5x Rambut Kaisar Siluman (Venena MetaCoenubia - Neo Plastida)
-  - 20x Kain Merah Sobek (Potum Bandit Gurun - Gurun Pasir Geist)
-  - 30x Kulit Karatan (Jasman - Reruntuhan Elban Urban)
+*93 => 94*
+- 5x Rambut Kaisar Siluman (Venena MetaCoenubia - Neo Plastida)
+- 20x Kain Merah Sobek (Potum Bandit Gurun - Gurun Pasir Geist)
+- 30x Kulit Karatan (Jasman - Reruntuhan Elban Urban)
 
-  *94 => 95*
-  Spina 300.000
+*94 => 95*
+- Spina 300.000
 
-  *95 => 96*
-  - 5x Tulang Pisteus (Pisteus - Pesisir Ducia:Area Terdalam)
-  - 20x Kain Phantom (Flooray - Dasar Tebing Lunagent/Desa Inje)
-  - 30X Bulu Berang-Berang Laut (Lutris - Pesisir Ducia)
+*95 => 96*
+- 5x Tulang Pisteus (Pisteus - Pesisir Ducia:Area Terdalam)
+- 20x Kain Phantom (Flooray - Dasar Tebing Lunagent/Desa Inje)
+- 30X Bulu Berang-Berang Laut (Lutris - Pesisir Ducia)
 
-  *96 => 97*
-  - 5x Sayap Arachnidemon (Arachnidemon- Lembah Arche:Area Terdalam)
-  - 20x Belenggu Logam (Besy - Lembah Arche)
-  - 30x Kulit Ular Aneh (Coofer - Reruntuhan Rokoko)
+*96 => 97*
+- 5x Sayap Arachnidemon (Arachnidemon- Lembah Arche:Area Terdalam)
+- 20x Belenggu Logam (Besy - Lembah Arche)
+- 30x Kulit Ular Aneh (Coofer - Reruntuhan Rokoko)
 
-  *97 => 98*
-  - 5x Jangat Berledir (Datuk Nezim - Lahan Basah Nezim)
-  - 20x Kain Enty (Enty - Rimbah Penyihir)
-  - 30x Poros Kokoh (Orang2an Sawah Seram - Rimbah Penyihir)
+*97 => 98*
+- 5x Jangat Berledir (Datuk Nezim - Lahan Basah Nezim)
+- 20x Kain Enty (Enty - Rimbah Penyihir)
+- 30x Poros Kokoh (Orang2an Sawah Seram - Rimbah Penyihir)
 
-  *98 => 99*
-  - 5x Perca Gendam Geni (Hexter - Rimbah Penyihir: Area Terdalam)
-  - 20x Piring Kappa (Kappadon - Lahan Basah Nezim)
-  - 30x Bulu Gagak (Orang2an Sawah Seram - Rimbah Penyihir)
+*98 => 99*
+- 5x Perca Gendam Geni (Hexter - Rimbah Penyihir: Area Terdalam)
+- 20x Piring Kappa (Kappadon - Lahan Basah Nezim)
+- 30x Bulu Gagak (Orang2an Sawah Seram - Rimbah Penyihir)
 
-  *99 => 100*
-  - 5x Inti Latebra Menggeliat (Trokostida - Nov Diela:A1)
-  - 20x Cairan lekat (Juvestida - Nov Diela)
-  - 30x Kulit Pelik (Mata Jahat - Padang Morga:A1)
+*99 => 100*
+- 5x Inti Latebra Menggeliat (Trokostida - Nov Diela:A1)
+- 20x Cairan lekat (Juvestida - Nov Diela)
+- 30x Kulit Pelik (Mata Jahat - Padang Morga:A1)
   `,
   mq: `
-  BAHAN MQ
-  - Daun Colon (Colon Leaf) x5
-  - Sisik Naga (Hard Dragon Skin) x2
-  - Daging Domba (Lamb Meat) x1
-  - Sulur (Vine) x3
-  - Paruh Tebal (Thick Beak) x3
-  - Sayap Peri (Fairy Feather) x3
-  - Koin Ksatria (Swordsman Stone Coin) x20
-  - Daging Tikus Pasir (Sand Mole Meat) x1
-  - Cakar binatang buas (Beast Claw) 3x
-  - Kulit Kodok Pasir (Sand Frog Skin) x5
-  - Taring Bergerigi (Jagged Fang) x10
-  - Kristal Saham (Saham Crystal) x5
-  - Permata Jiwa (Spiritual Gemstone) x1
-  - Anggur Rokoko (Rokoko Grape) x5
-  - Kayu Labilans (Labilans Wood) x10
-  - Tanduk Patah ( Broken Horn) x20
-  - Biji Berkembang (Growing Ore ) x5
-  - Batu Jabali (Jabali Stone) x5
-  - Kulit Tebal Berlendir (Slimy Thick Skin) x20
-  - Akar Melilit (Tangled Roots) x10
-  - Kayu Menabra (Menabra Wood) x10
-  - Pupuk Keruh (Stagnant Fertilizer) x10
-
+BAHAN MQ
+- Daun Colon (Colon Leaf) x5
+- Sisik Naga (Hard Dragon Skin) x2
+- Daging Domba (Lamb Meat) x1
+- Sulur (Vine) x3
+- Paruh Tebal (Thick Beak) x3
+- Sayap Peri (Fairy Feather) x3
+- Koin Ksatria (Swordsman Stone Coin) x20
+- Daging Tikus Pasir (Sand Mole Meat) x1
+- Cakar binatang buas (Beast Claw) 3x
+- Kulit Kodok Pasir (Sand Frog Skin) x5
+- Taring Bergerigi (Jagged Fang) x10
+- Kristal Saham (Saham Crystal) x5
+- Permata Jiwa (Spiritual Gemstone) x1
+- Anggur Rokoko (Rokoko Grape) x5
+- Kayu Labilans (Labilans Wood) x10
+- Tanduk Patah ( Broken Horn) x20
+- Biji Berkembang (Growing Ore ) x5
+- Batu Jabali (Jabali Stone) x5
+- Kulit Tebal Berlendir (Slimy Thick Skin) x20
+- Akar Melilit (Tangled Roots) x10
+- Kayu Menabra (Menabra Wood) x10
+- Pupuk Keruh (Stagnant Fertilizer) x10
   `,
 };
 const thumbnails = (config?.thumbnail ?? []).filter(Boolean);
