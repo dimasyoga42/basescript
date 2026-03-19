c6e1235ca20386ad80f4ad05ca608591455a5b5b
dimasyoga42 <dimasyoga9090@gmail.com>
2026-03-19 18:05:22 +0700

    feat: add search item

---
+++ plugins/toram/item.js
@@ -0,0 +1,34 @@
+import { config } from "../../config.js";
+import { sendText } from "../../src/config/message.js";
+import { supa } from "../../src/config/supa.js";
+
+const handler = async (m, { conn }) => {
+  try {
+    const name = m.text.replace(/\.item/, "").trim();
+    if (!name)
+      return sendText(
+        conn,
+        m.chat,
+        `${config.message.invalid}\nuse: .item name`,
+        m,
+      );
+    const { data } = await supa
+      .from("item")
+      .select("nama, jenis, stat, drop")
+      .ilike("nama", `%${name}%`);
+
+    if (!data || data.length === 0)
+      return sendText(conn, m.chat, config.message.notFound, m);
+
+    const mtext = data
+      .map(
+        (item) =>
+          `${item.nama}(${item.jenis})\nstat:\n${item.stat}\ndrop:\n${item.drop}\n`,
+      )
+      .join("\n────────────────────────\n");
+
+    sendText(conn, m.chat, mtext, m);
+  } catch (err) {
+    sendText(conn, m.chat, err.message, m);
+  }
+};
