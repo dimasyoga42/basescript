592e9142efdeb90190fd79e5eb77b224082efc10
dimasyoga42 <dimasyoga9090@gmail.com>
2026-04-07 14:27:55 +0700

    refector command

--- plugins/toram/ability.js
+++ plugins/toram/ability.js
@@ -1,63 +1,44 @@
 import { config, thumbnail } from "../../config.js";
-import { sendFancyText, sendText } from "../../src/config/message.js";
+import { sendImage, sendText } from "../../src/config/message.js";
 import { supa } from "../../src/config/supa.js";
 
 const handler = async (m, { conn }) => {
   try {
-    const parts = m.text.trim().split(/\s+/);
-    const query = parts.slice(1).join(" ").trim();
-    console.log(query);
-    if (!query)
-      return sendFancyText(conn, m.chat, {
-        title: config.BotName,
-        body: "Contoh: .ability nama",
-        thumbnail,
-        text: config.message.invalid,
-        msg: m,
-      });
+    const name = m.text.replace(/\.appview|\.app/, "").trim();
+    if (!name) return sendText(conn, m.chat, config.message.invalid, m);
 
     const { data, error } = await supa
-      .from("ability")
-      .select("*")
-      .ilike("name", `%${query}%`);
+      .from("appview")
+      .select("name, image_url")
+      .ilike("name", `%${name}%`);
 
     if (error || !data || data.length === 0)
       return sendText(conn, m.chat, config.message.notFound, m);
 
     if (data.length === 1) {
-      const item = data[0];
-      return sendText(conn, m.chat, `*${item.name}*\n\n${item.stat_effect}`, m);
+      return sendImage(conn, m.chat, data[0].image_url, data[0].name, m);
     }
 
     await conn.sendButton(m.chat, {
-      image: thumbnail, // ❗ jangan pakai { url: thumbnail }
-      caption: "Pilih ability untuk melihat detail:",
+      image: thumbnail,
+      caption: "Pilih Appview yang tersedia",
       footer: config.OwnerName,
       buttons: data.map((item) => ({
         name: "quick_reply",
         buttonParamsJson: JSON.stringify({
           display_text: item.name,
-          id: `.trait ${item.name}`,
+          id: `.appview ${item.name}`,
         }),
       })),
-      bottom_sheet: true, //kalau mau button dalam button
-      bottom_name: "menu Ability",
+      bottom_sheet: true,
+      bottom_name: "menu Appview",
     });
   } catch (err) {
-    console.error("[ability]", err.message);
-    await sendFancyText(conn, m.chat, {
-      title: config.BotName,
-      body: `Developer By ${config.OwnerName}`,
-      thumbnail,
-      text: config.message.error,
-      msg: m,
-    });
+    sendText(conn, m.chat, config.message.error, m);
   }
 };
 
-handler.command = "trait";
-handler.alias = ["ability"];
+handler.command = "appview";
+handler.alias = ["app"];
 handler.category = "Toram Search";
-handler.submenu = "Toram";
-
 export default handler;
