b1559aa41eb796e8e4f34fa543300023ff6165e9
dimasyoga42 <dimasyoga9090@gmail.com>
2026-04-16 13:09:47 +0700

    penambahan checing pada level

--- src/config/redis.js
+++ src/config/redis.js
@@ -3,8 +3,8 @@ import { createClient } from "redis";
 export const client = createClient();
 client.on("err", (err) => console.log("redis error" + err));
 
-const run = async () {
-  await client.connect()
-}
+const run = async () => {
+  await client.connect();
+};
 
-run()
+run();
