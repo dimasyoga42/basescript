311ca508b266a621d82dd987f4adb8a519fe652c
dimasyoga42 <dimasyoga9090@gmail.com>
2026-04-25 02:48:21 +0700

    perbaikan  all

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { config, thumbnail } from "../config.js";
 import {
   sendFancyText,
@@ -23,25 +22,28 @@ const handler = async (m, { conn }) => {
     if (!categories[cat]) categories[cat] = [];
     categories[cat].push(...cmds);
   }
-  const Namemesage = m.pushName;
 
   let result =
     "*Cara Penggunaan:*\n- gunakan .menu / .help untuk menampilkan menu bot\n- pilih salah satu fitur yang anda ingin gunakan\n- jika anda tidak paham cara penggunaan ketikan saja command yang anda ingin gunakan nanti bot akan memberikan intruksi penggunaan\n- gunakan .saran untuk memberikan masukan\n\n *Aturan Penggunaan:*\n- Dilarang Keras Spam command di batas tidak wajar\n- Dilarang Menggunakan tools stiker brat pin untuk hal hal kurang etis (vulgar, rasis, dll) melanggar banned\n";
+
   for (const cat in categories) {
     result += `\n╭─ *${cat}*\n`;
     result += categories[cat].map((c) => `│.${c}`).join("\n") + "\n╰────\n";
   }
-  const image = await axios.get(`${config.restapi.toram}etc/waifu`);
-  const img = image.data.result;
+
+  const randomThumb =
+    config.thumbnail[Math.floor(Math.random() * config.thumbnail.length)];
+
   await sendMenu(conn, m.chat, {
     title: m.pushName,
     body: "Neura Inc Development",
     text: result.trim(),
-    thumbnail: img.Url,
+    thumbnail: randomThumb,
     quoted: m,
   });
 };
 
 handler.command = ["menu", "help"];
 handler.category = "Menu Grub";
+
 export default handler;
