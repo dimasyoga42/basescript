a6ba0a790491e3baa2a53cc8df831d7c321536e5
dimasyoga42 <dimasyoga9090@gmail.com>
2026-05-03 21:24:32 +0700

    penambahan cornlive

--- plugins/tools/liveNotif.js
+++ plugins/tools/liveNotif.js
@@ -1,6 +1,7 @@
 import { getUserData, saveUserData } from "../../src/config/func.js";
 import path from "path";
 import { isAdmin } from "../_function/_admin.js";
+import { sendMenu } from "../../src/config/message.js";
 
 const db_path = path.resolve("db", "live.json");
 
@@ -16,8 +17,10 @@ const handler = async (m, { conn }) => {
         status: false,
       };
       db.push(grubEntry);
+      conn.sendMessage(m.chat, { text: `corn live dihidupkan` }, { quoted: m });
     }
     grubEntry.status = !grubEntry.status;
+    conn.sendMessage(m.chat, { text: `corn live dimatikan` }, { quoted: m });
     saveUserData(db_path, db);
   } catch (err) {
     console.error("[live] Error:", err.message);
