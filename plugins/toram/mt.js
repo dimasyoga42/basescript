c6ca9e70aabc921beff0fb87b46610ded17ec085
dimasyoga42 <dimasyoga9090@gmail.com>
2026-05-11 00:23:45 +0700

    perbaikan all message

--- plugins/toram/mt.js
+++ plugins/toram/mt.js
@@ -19,7 +19,7 @@ const handler = async (m, { conn }) => {
     const soup = cheerio.load(des.data);
     const result = soup("#news").find("div").text().trim();
     const reg = result.split("Kembali ke atas")[0];
-    sendText(conn, m.chat, `${reg}`, m);
+    await sendText(conn, m.chat, `${reg}`, m);
     // sendBtns(conn, m.chat, {
     //   title: m.pushName,
     //   body: "Neura Inc",
