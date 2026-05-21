ecc3c153413ee55b744883ad7612a71206be012d
dimasyoga42 <dimasyoga9090@gmail.com>
2026-05-21 10:06:47 +0700

    perbaikan nonaktif fill

--- plugins/toram/fillarm.js
+++ plugins/toram/fillarm.js
@@ -1,77 +1,77 @@
-import axios from "axios";
-import { config, thumbnail } from "../../config.js";
-import {
-  sendFancyText,
-  sendFancyTextModif,
-  sendText,
-} from "../../src/config/message.js";
-const handler = async (m, { conn }) => {
-  try {
-    const stats = m.text.replace(".fillarm", "");
-    if (!stats)
-      return sendFancyText(conn, m.chat, {
-        title: config.BotName,
-        body: "exemple: .fillarm dte%=max, atk%=10, cd=20, cr=25, acc%=min, matk%=min, mp%=min, lv320, pot112, bs300",
-        thumbnail: thumbnail,
-        text: "gunakan .stats untuk melihat stat yang tersida",
-        quoted: m,
-      });
-    const url = `https://neurapi.mochinime.cyou/api/toram/filarm?text=${encodeURIComponent(stats)}`;
-    const { data } = await axios.get(url, { timeout: 15000 });
-    if (!data?.ok || !data?.hasValidResult)
-      return sendFancyText(conn, m.chat, {
-        title: config.BotName,
-        body: `develop by ${config.OwnerName}`,
-        thumbnail: thumbnail,
-        text: config.message.notFound,
-        quoted: m,
-      });
+// import axios from "axios";
+// import { config, thumbnail } from "../../config.js";
+// import {
+//   sendFancyText,
+//   sendFancyTextModif,
+//   sendText,
+// } from "../../src/config/message.js";
+// const handler = async (m, { conn }) => {
+//   try {
+//     const stats = m.text.replace(".fillarm", "");
+//     if (!stats)
+//       return sendFancyText(conn, m.chat, {
+//         title: config.BotName,
+//         body: "exemple: .fillarm dte%=max, atk%=10, cd=20, cr=25, acc%=min, matk%=min, mp%=min, lv320, pot112, bs300",
+//         thumbnail: thumbnail,
+//         text: "gunakan .stats untuk melihat stat yang tersida",
+//         quoted: m,
+//       });
+//     const url = `https://neurapi.mochinime.cyou/api/toram/filarm?text=${encodeURIComponent(stats)}`;
+//     const { data } = await axios.get(url, { timeout: 15000 });
+//     if (!data?.ok || !data?.hasValidResult)
+//       return sendFancyText(conn, m.chat, {
+//         title: config.BotName,
+//         body: `develop by ${config.OwnerName}`,
+//         thumbnail: thumbnail,
+//         text: config.message.notFound,
+//         quoted: m,
+//       });
 
-    const steps = data.steps.join("\n");
+//     const steps = data.steps.join("\n");
 
-    const positiveStats =
-      data.inputConfig.positiveStats.length > 0
-        ? data.inputConfig.positiveStats
-            .map((v) => `• ${v.name} (${v.level})`)
-            .join("\n")
-        : "-";
+//     const positiveStats =
+//       data.inputConfig.positiveStats.length > 0
+//         ? data.inputConfig.positiveStats
+//             .map((v) => `• ${v.name} (${v.level})`)
+//             .join("\n")
+//         : "-";
 
-    const negativeStats =
-      data.inputConfig.negativeStats.length > 0
-        ? data.inputConfig.negativeStats
-            .map((v) => `• ${v.name} (${v.level})`)
-            .join("\n")
-        : "-";
+//     const negativeStats =
+//       data.inputConfig.negativeStats.length > 0
+//         ? data.inputConfig.negativeStats
+//             .map((v) => `• ${v.name} (${v.level})`)
+//             .join("\n")
+//         : "-";
 
-    const material = Object.entries(data.materialDetails)
-      .filter(([k]) => k !== "reduction")
-      .map(([k, v]) => `${k.toUpperCase().padEnd(8)}: ${v}`)
-      .join("\n");
+//     const material = Object.entries(data.materialDetails)
+//       .filter(([k]) => k !== "reduction")
+//       .map(([k, v]) => `${k.toUpperCase().padEnd(8)}: ${v}`)
+//       .join("\n");
 
-    const result =
-      `source:http://tanaka0.work/\nSuccess Rate : ${data.successRate}\nStarting Pot : ${data.startingPot}\nMaterial Cost\n${material}\nReduction         : ${data.materialDetails.reduction}\nHighest Step Cost : ${data.highestStepCost}\n
+//     const result =
+//       `source:http://tanaka0.work/\nSuccess Rate : ${data.successRate}\nStarting Pot : ${data.startingPot}\nMaterial Cost\n${material}\nReduction         : ${data.materialDetails.reduction}\nHighest Step Cost : ${data.highestStepCost}\n
 
-   Steps (${data.totalSteps})\n${steps}\n\nCharacter Config\nCharacter Lv : ${data.inputConfig.characterLevel}\nBS Lv        : ${data.inputConfig.professionLevel}\nStart Pot    : ${data.inputConfig.startingPotential}
+//    Steps (${data.totalSteps})\n${steps}\n\nCharacter Config\nCharacter Lv : ${data.inputConfig.characterLevel}\nBS Lv        : ${data.inputConfig.professionLevel}\nStart Pot    : ${data.inputConfig.startingPotential}
 
-   Process Time : ${data.duration} ms
-   `.trim();
-    // await sendFancyText(conn, m.chat, {
-    //   title: config.BotName,
-    //   body: `Develop by ${config.OwnerName}`,
-    //   thumbnail: thumbnail,
-    //   text: result,
-    //   quoted: m,
-    // });
-    sendFancyTextModif(conn, m.chat, {
-      name: m.pushName,
-      caption: result,
-      image: thumbnail,
-      quoted: m,
-    });
-  } catch (err) {}
-};
+//    Process Time : ${data.duration} ms
+//    `.trim();
+//     // await sendFancyText(conn, m.chat, {
+//     //   title: config.BotName,
+//     //   body: `Develop by ${config.OwnerName}`,
+//     //   thumbnail: thumbnail,
+//     //   text: result,
+//     //   quoted: m,
+//     // });
+//     sendFancyTextModif(conn, m.chat, {
+//       name: m.pushName,
+//       caption: result,
+//       image: thumbnail,
+//       quoted: m,
+//     });
+//   } catch (err) {}
+// };
 
-handler.command = ["fillarm"];
-handler.category = "Toram Simulator";
-handler.submenu = "Toram";
-export default handler;
+// handler.command = ["fillarm"];
+// handler.category = "Toram Simulator";
+// handler.submenu = "Toram";
+// export default handler;
--- plugins/toram/fillwep.js
+++ plugins/toram/fillwep.js
@@ -1,70 +1,70 @@
-import axios from "axios";
-import { config, thumbnail } from "../../config.js";
-import { sendFancyText, sendText } from "../../src/config/message.js";
-const handler = async (m, { conn }) => {
-  try {
-    const stats = m.text.replace(".fillwep", "");
-    if (!stats)
-      return sendText(
-        conn,
-        m.chat,
-        `gunakan .stats untuk melihat stat yang tersedia`,
-      );
-    const url = `https://neurapi.mochinime.cyou/api/toram/filwep?text=${encodeURIComponent(stats)}`;
-    const { data } = await axios.get(url, { timeout: 15000 });
-    if (!data?.ok || !data?.hasValidResult)
-      return sendFancyText(conn, m.chat, {
-        title: config.BotName,
-        body: `develop by ${config.OwnerName}`,
-        thumbnail: thumbnail,
-        text: config.message.notFound,
-        quoted: m,
-      });
+// import axios from "axios";
+// import { config, thumbnail } from "../../config.js";
+// import { sendFancyText, sendText } from "../../src/config/message.js";
+// const handler = async (m, { conn }) => {
+//   try {
+//     const stats = m.text.replace(".fillwep", "");
+//     if (!stats)
+//       return sendText(
+//         conn,
+//         m.chat,
+//         `gunakan .stats untuk melihat stat yang tersedia`,
+//       );
+//     const url = `https://neurapi.mochinime.cyou/api/toram/filwep?text=${encodeURIComponent(stats)}`;
+//     const { data } = await axios.get(url, { timeout: 15000 });
+//     if (!data?.ok || !data?.hasValidResult)
+//       return sendFancyText(conn, m.chat, {
+//         title: config.BotName,
+//         body: `develop by ${config.OwnerName}`,
+//         thumbnail: thumbnail,
+//         text: config.message.notFound,
+//         quoted: m,
+//       });
 
-    const steps = data.steps.join("\n");
+//     const steps = data.steps.join("\n");
 
-    const positiveStats =
-      data.inputConfig.positiveStats.length > 0
-        ? data.inputConfig.positiveStats
-            .map((v) => `• ${v.name} (${v.level})`)
-            .join("\n")
-        : "-";
+//     const positiveStats =
+//       data.inputConfig.positiveStats.length > 0
+//         ? data.inputConfig.positiveStats
+//             .map((v) => `• ${v.name} (${v.level})`)
+//             .join("\n")
+//         : "-";
 
-    const negativeStats =
-      data.inputConfig.negativeStats.length > 0
-        ? data.inputConfig.negativeStats
-            .map((v) => `• ${v.name} (${v.level})`)
-            .join("\n")
-        : "-";
+//     const negativeStats =
+//       data.inputConfig.negativeStats.length > 0
+//         ? data.inputConfig.negativeStats
+//             .map((v) => `• ${v.name} (${v.level})`)
+//             .join("\n")
+//         : "-";
 
-    const material = Object.entries(data.materialDetails)
-      .filter(([k]) => k !== "reduction")
-      .map(([k, v]) => `${k.toUpperCase().padEnd(8)}: ${v}`)
-      .join("\n");
+//     const material = Object.entries(data.materialDetails)
+//       .filter(([k]) => k !== "reduction")
+//       .map(([k, v]) => `${k.toUpperCase().padEnd(8)}: ${v}`)
+//       .join("\n");
 
-    const result = `
-    Success Rate : ${data.successRate}\nStarting Pot : ${data.startingPot}
+//     const result = `
+//     Success Rate : ${data.successRate}\nStarting Pot : ${data.startingPot}
 
-   Positive Stats\n${positiveStats}
+//    Positive Stats\n${positiveStats}
 
-   Negative Stats\n${negativeStats}
+//    Negative Stats\n${negativeStats}
 
-   Steps (${data.totalSteps})\n${steps}\n\nMaterial Cost\n${material}\nReduction         : ${data.materialDetails.reduction}\nHighest Step Cost : ${data.highestStepCost}\n\nCharacter Config\nCharacter Lv : ${data.inputConfig.characterLevel}\nBS Lv        : ${data.inputConfig.professionLevel}\nStart Pot    : ${data.inputConfig.startingPotential}
+//    Steps (${data.totalSteps})\n${steps}\n\nMaterial Cost\n${material}\nReduction         : ${data.materialDetails.reduction}\nHighest Step Cost : ${data.highestStepCost}\n\nCharacter Config\nCharacter Lv : ${data.inputConfig.characterLevel}\nBS Lv        : ${data.inputConfig.professionLevel}\nStart Pot    : ${data.inputConfig.startingPotential}
 
-   Process Time : ${data.duration} ms
-   `.trim();
-    // await sendFancyText(conn, m.chat, {
-    //   title: config.BotName,
-    //   body: `Develop by ${config.OwnerName}`,
-    //   thumbnail: thumbnail,
-    //   text: result,
-    //   quoted: m,
-    // });
-    sendText(conn, m.chat, result, m);
-  } catch (err) {}
-};
+//    Process Time : ${data.duration} ms
+//    `.trim();
+//     // await sendFancyText(conn, m.chat, {
+//     //   title: config.BotName,
+//     //   body: `Develop by ${config.OwnerName}`,
+//     //   thumbnail: thumbnail,
+//     //   text: result,
+//     //   quoted: m,
+//     // });
+//     sendText(conn, m.chat, result, m);
+//   } catch (err) {}
+// };
 
-handler.command = ["fillwep"];
-handler.category = "Toram Simulator";
-handler.submenu = "Toram";
-export default handler;
+// handler.command = ["fillwep"];
+// handler.category = "Toram Simulator";
+// handler.submenu = "Toram";
+// export default handler;
