230efeb92172a921b1d169980d3b6e43566b5535
dimasyoga42 <dimasyoga9090@gmail.com>
2026-06-21 08:56:07 +0700

    maid

--- plugins/fun/maid.js
+++ plugins/fun/maid.js
@@ -8,18 +8,38 @@ import {
 
 const handler = async (m, { conn }) => {
   try {
-    const res = await axios.get(`${config.restapi.maid}`);
+    const res = await axios.get(
+      `https://neurapi.mochinime.cyou/api/etc/pixiv/search?query=maid`,
+    );
     const data = res.data;
-    const image = data.items[0].url;
-    console.log(image);
+
+    if (
+      !data?.success ||
+      !Array.isArray(data?.data) ||
+      data.data.length === 0
+    ) {
+      return sendText(conn, m.chat, "gagal nih harap di ulang", m);
+    }
+
+    const items = data.data;
+    const randomItem = items[Math.floor(Math.random() * items.length)];
+
+    if (!randomItem?.thumbnail) {
+      return sendText(conn, m.chat, "gagal nih harap di ulang", m);
+    }
+
+    const cleanedThumbnail = randomItem.thumbnail
+      .replace("/c/250x250_80_a2/img-master/", "/img-master/")
+      .replace("/c/250x250_80_a2/custom-thumb/", "/img-master/");
+
+    const image = `https://neurapi.mochinime.cyou/api/etc/pixiv/image?url=${cleanedThumbnail}`;
 
     sendImage(conn, m.chat, image, "ini adalah maid kesukaan mu", m);
   } catch (err) {
     sendText(conn, m.chat, "gagal nih harap di ulang", m);
   }
 };
-
-// handler.command = ["maid"];
-// handler.category = "Menu Fun";
-// handler.submenu = "Fun";
-// export default handler;
+handler.command = ["maid"];
+handler.category = "Menu Fun";
+handler.submenu = "Fun";
+export default handler;
