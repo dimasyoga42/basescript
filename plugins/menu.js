4e2d0d2ad2b228c71ea442c88c9671822414eceb
dimasyoga42 <dimasyoga9090@gmail.com>
2026-06-24 08:02:01 +0700

    perubahan image menu

--- plugins/menu.js
+++ plugins/menu.js
@@ -4,7 +4,7 @@ import { sendFancyText, sendFancyTextModif } from "../src/config/message.js";
 import { supa } from "../src/config/supa.js";
 import { buildAvaGrid } from "./_function/_format.js";
 import axios from "axios";
-//import { demoButtonV2 } from "../src/config/ms.js";
+import { demoButtonV2 } from "../src/config/ms.js";
 
 const handler = async (m, { conn }) => {
   const image = await buildAvaGrid(
@@ -80,12 +80,21 @@ const handler = async (m, { conn }) => {
   const randomThumb =
     config.thumbnail[Math.floor(Math.random() * config.thumbnail.length)];
 
-  await sendFancyTextModif(conn, m.chat, {
-    name: m.pushName,
-    image: randomThumb,
-    caption: result.trim(),
-    quoted: m,
-  });
+  // await sendFancyTextModif(conn, m.chat, {
+  //   name: m.pushName,
+  //   image: randomThumb,
+  //   caption: result.trim(),
+  //   quoted: m,
+  // });
+  await demoButtonV2(
+    conn,
+    m,
+    "Neura Sama",
+    "a",
+    "dimasyoga",
+    "siap Melayani anda",
+    randomThumb,
+  );
 };
 handler.command = "menu";
 handler.alias = ["help"];
