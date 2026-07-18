93df82a5d05ac2dafadd06b24e0881d675a31f9d
dimasyoga42 <dimasyoga9090@gmail.com>
2026-07-18 21:54:15 +0700

    delete waifu and change catgirl in bot

--- plugins/fun/milf.js
+++ plugins/fun/milf.js
@@ -10,37 +10,17 @@ const handler = async (m, { conn }) => {
   try {
     const randomPage = Math.floor(Math.random() * 60) + 1;
     const res = await axios.get(
-      `https://neurapi.mochinime.cyou/api/etc/pixiv/search?query=MILF&page=${randomPage}`,
+      `https://api.nekosapi.com/v4/images/random?rating=safe&limit=1&tags=catgirl`,
     );
     const data = res.data;
     console.log(data);
-    if (
-      !data?.success ||
-      !Array.isArray(data?.data) ||
-      data.data.length === 0
-    ) {
-      return sendText(conn, m.chat, "gagal nih harap di ulang", m);
-    }
 
-    const items = data.data;
-    const randomItem = items[Math.floor(Math.random() * items.length)];
-
-    if (!randomItem?.thumbnail) {
-      return sendText(conn, m.chat, "gagal nih harap di ulang", m);
-    }
-
-    const cleanedThumbnail = randomItem.thumbnail
-      .replace("/c/250x250_80_a2/img-master/", "/img-master/")
-      .replace("/c/250x250_80_a2/custom-thumb/", "/img-master/");
-
-    const image = `https://neurapi.mochinime.cyou/api/etc/pixiv/image?url=${cleanedThumbnail}`;
-
-    sendImage(conn, m.chat, image, "ini adalah milf kesukaan mu", m);
+  sendImage(conn, m.chat, data[0].url, "ini adalah ras kucing kesukaan mu", m);
   } catch (err) {
     sendText(conn, m.chat, "gagal nih harap di ulang", m);
   }
 };
-handler.command = ["milf"];
+handler.command = ["catgirl"];
 handler.category = "Menu Fun";
 handler.submenu = "Fun";
 export default handler;
