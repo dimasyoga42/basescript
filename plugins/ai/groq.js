08cc7dce0c83d70560ba3a8598e229504c70212c
dimasyoga42 <dimasyoga9090@gmail.com>
2026-07-30 12:09:33 +0700

    perbaikan

--- plugins/ai/groq.js
+++ plugins/ai/groq.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 import { OpenRouter } from "@openrouter/sdk";
 import { getUserData, saveUserData } from "../../src/config/func.js";
 import ChatEngine from "./neuraAI/ai/chatengine.js";
-import { runTools } from "./neuraAI/tools/toolRouter.js";
 import { Ollama } from "ollama";
+import { runTools } from "./neuraAI/tools/toolsRouter.js";
 
 dotenv.config();
 
