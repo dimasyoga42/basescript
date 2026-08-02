e5c8c0027c0892c2bcdf251765d001d8d142e007
dimasyoga42 <dimasyoga9090@gmail.com>
2026-08-02 11:55:23 +0700

    update

--- plugins/ai/neuraAI/tools/toramTools.js
+++ plugins/ai/neuraAI/tools/toramTools.js
@@ -1,11 +1,29 @@
-import { supa } from "../../../../src/config/supa.js"
+import { supa } from "../../../../src/config/supa.js";
 
 export const xtalFinder = async (name) => {
   try {
-    const db = await supa.from("xtal").select("name, type, upgrade_rute, stats, max_upgrade_route").ilike("name", name)
-    if (!db) return "xtal tidak ada"
-    return db.data.map((item) => `${item.name}\n *${item.type}*\n Stat Effect:\n ${item.stats}\n Rute:\n- ${item.upgrade_rute | "-"}\n- ${item.max_upgrade_route | "-"}`)
+    const { data, error } = await supa
+      .from("xtal")
+      .select("name, type, upgrade_rute, stats, max_upgrade_route")
+      .ilike("name", `%${name}%`).limit(1);
+
+    if (error) throw error;
+
+    if (!data || data.length === 0) {
+      return "xtal tidak ada";
+    }
+
+    return data.map(
+      (item) => `${item.name}
+*${item.type}*
+Stat Effect:
+${item.stats}
+
+Rute:
+- ${item.upgrade_rute ?? "-"}
+- ${item.max_upgrade_route ?? "-"}`
+    );
   } catch (err) {
-    throw new err
+    throw err;
   }
-}
+};
