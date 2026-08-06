8467962529706e52376135dedd778da12219e437
dimasyoga42 <dimasyoga9090@gmail.com>
2026-08-06 15:31:30 +0700

    update: penambahan tumbnail

--- plugins/menu.js
+++ plugins/menu.js
@@ -117,7 +117,7 @@ const handler = async (m, { conn }) => {
   //   config.msgtxt[Math.floor(Math.random() * config.msgtxt.length)],
   //   randomThumb,
   // );
-  await thumb(conn, m, config.BotName, result, config.OwnerName, config.msgtxt[Math.floor(Math.random() * config.msgtxt.length)], randomThumb)
+  await thumb(conn, m, config.BotName, result, config.OwnerName, config.msgtxt[Math.floor(Math.random() * config.msgtxt.length)], "https://server.neurasama.my.id/etc/thumbnail")
 };
 
 handler.command = "menu";
