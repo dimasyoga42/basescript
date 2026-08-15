f4da7fb2cb0f1878a139a2817445fc92d98a6bfd
dimasyoga42 <dimasyoga9090@gmail.com>
2026-08-15 21:55:22 +0700

    penambahan menu jb

--- plugins/jb/cekrate.js
+++ plugins/jb/cekrate.js
@@ -5,9 +5,9 @@ const handler = async (m, {conn}) => {
   try {
     let data = await getUserData(db, (item) => item.id === m.chat)
     if (!data || !data.rate) {
-      return conn.sendMessage(m.chat, { text: "belum ada rate yang di set di chat ini, gunakan .setrate 100-150" }, { quoted: m })
+      return conn.sendMessage(m.chat, { text: "belum ada rate spina yang di set di chat ini, gunakan .setrate 100-150" }, { quoted: m })
     }
-    return conn.sendMessage(m.chat, { text: `rate saat ini: *${data.rate}*` }, { quoted: m })
+    return conn.sendMessage(m.chat, { text: `rate saat ini: *${data.rate}M*` }, { quoted: m })
   } catch (err) {
     return conn.sendMessage(m.chat, {text: "terjadi kesalahan dalam mengambil database"}, {quoted: m})
   }
