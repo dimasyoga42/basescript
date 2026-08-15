c2483ee0c1944600ebedfeca830774a9dd290e30
dimasyoga42 <dimasyoga9090@gmail.com>
2026-08-15 21:54:46 +0700

    penambahan menu jb

---
+++ plugins/jb/cekrate.js
@@ -0,0 +1,18 @@
+import { getUserData, saveUserData } from "../../src/config/func.js"
+import path from "path"
+const db = path.resolve("db", "rate.json")
+const handler = async (m, {conn}) => {
+  try {
+    let data = await getUserData(db, (item) => item.id === m.chat)
+    if (!data || !data.rate) {
+      return conn.sendMessage(m.chat, { text: "belum ada rate yang di set di chat ini, gunakan .setrate 100-150" }, { quoted: m })
+    }
+    return conn.sendMessage(m.chat, { text: `rate saat ini: *${data.rate}*` }, { quoted: m })
+  } catch (err) {
+    return conn.sendMessage(m.chat, {text: "terjadi kesalahan dalam mengambil database"}, {quoted: m})
+  }
+}
+handler.command = "cakrate"
+handler.category = "Menu Jb"
+handler.alias = "ceckrate"
+export default handler
---
+++ plugins/jb/rate.js
@@ -0,0 +1,60 @@
+import { getUserData, saveUserData } from "../../src/config/func.js"
+import path from "path"
+import { isAdmin } from "../_function/_admin.js";
+
+const db = path.resolve("db", "rate.json")
+
+const handler = async (m, { conn }) => {
+  try {
+    if (!(await isAdmin(conn, m))) return;
+    const vel = m.text.replace(/\.setrate/i, "").trim()
+    if (!vel) {
+      return conn.sendMessage(
+        m.chat,
+        { text: "rate harus di isi tidak boleh kosong gunakan .setrate 100-150" },
+        { quoted: m }
+      )
+    }
+
+    // validasi format "angka-angka"
+    if (!/^\d+-\d+$/.test(vel)) {
+      return conn.sendMessage(
+        m.chat,
+        { text: "format salah, gunakan contoh: .setrate 100-150" },
+        { quoted: m }
+      )
+    }
+
+    let data = await getUserData(db, (item) => item.id === m.chat)
+
+    if (!data) {
+      const newData = {
+        id: m.chat,
+        rate: vel,
+      }
+      await saveUserData(db, newData)
+    } else {
+      data.rate = vel
+      await saveUserData(db, data)
+    }
+
+    return conn.sendMessage(
+      m.chat,
+      { text: `rate berhasil diatur ke *${vel}*` },
+      { quoted: m }
+    )
+  } catch (err) {
+    console.log(err)
+    return conn.sendMessage(
+      m.chat,
+      { text: "terjadi kesalahan dalam memperbarui database" },
+      { quoted: m }
+    )
+  }
+}
+
+handler.command = "setrate"
+handler.category = "Menu Jb"
+handler.alias = "srate"
+
+export default handler
