f17f28de209cb0fa93dfe583b698f264ec3a2a18
dimasyoga42 <dimasyoga9090@gmail.com>
2026-09-11 18:55:01 +0700

    perbaikan profile

--- plugins/groups/profile/setpp.js
+++ plugins/groups/profile/setpp.js
@@ -55,7 +55,7 @@ const handler = async (m, { conn }) => {
 
     const { error } = await supa
       .from("profile")
-      .upsert({ user_id: userId, profile_path: filePath });
+      .upsert({ user_id: userId, profile_path: filePath }, { onConflict: "user_id" });
 
     if (error) throw error;
 
