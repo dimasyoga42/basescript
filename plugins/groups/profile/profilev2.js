35542e7053b4c9bf3488e81381b5cff56241c00a
dimasyoga42 <dimasyoga9090@gmail.com>
2026-09-13 10:44:04 +0700

    fix profile

--- plugins/groups/profile/profilev2.js
+++ plugins/groups/profile/profilev2.js
@@ -44,7 +44,7 @@ const handler = async (m, { conn }) => {
     const { data, error } = await supa
       .from("profile")
       .select("user_id, bio, profile_path")
-      .eq("user_id", `${targetId}`).maybeSingle()
+      .eq("user_id", `${targetId}`)
     console.log(data, targetId)
     if (error) throw error
 
