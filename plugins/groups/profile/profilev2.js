bab39c93dbcc82fef9febf5068816d1decc34409
dimasyoga42 <dimasyoga9090@gmail.com>
2026-09-13 10:26:20 +0700

    fix profile

--- plugins/groups/profile/profilev2.js
+++ plugins/groups/profile/profilev2.js
@@ -33,7 +33,7 @@ const handler = async (m, { conn }) => {
     const { data, error } = await supa
       .from("profile")
       .select("user_id, bio, profile_path")
-      .eq("user_id", targetId).single()
+      .ilike("user_id", `%${targetId}`).single()
 
     if (error)
       throw error
